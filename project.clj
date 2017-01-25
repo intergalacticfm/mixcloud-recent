79adc33f3e1f02f1f6f6b70432278bf0207fdc17
dan <dan@MacBook-Pro-6.local>
2017-01-25 17:45:42 -0500

    update to latest clj/cljs deps

--- project.clj
+++ project.clj
@@ -1,22 +1,22 @@
 (defproject ifmmxcl "0.1.0-SNAPSHOT"
   :description "get recent mixcloud mixes"
   :url "http://intergalacticfm.net"
-  :dependencies [[com.taoensso/timbre            "4.3.1"]
-                 [funcool/promesa                "1.1.1"]
+  :dependencies [[com.taoensso/timbre            "4.7.4"]
+;;                 [funcool/promesa                "1.1.1"]
                  [org.clojure/clojure            "1.8.0"]
-                 [org.clojure/clojurescript      "1.8.34"]
-                 [org.clojure/core.async         "0.2.374"]
-                 [io.nervous/cljs-lambda         "0.3.0"]
-                 [io.nervous/cljs-nodejs-externs "0.2.0"]
-                 [io.nervous/eulalie             "0.6.8-SNAPSHOT"]
+                 [org.clojure/clojurescript      "1.8.51"]
+                 [org.clojure/core.async         "0.2.395"]
+                 [io.nervous/cljs-lambda         "0.3.3"]
+;;                 [io.nervous/cljs-nodejs-externs "0.2.0"]
+                 [io.nervous/eulalie             "0.6.10"]
                  [io.nervous/glossop             "0.2.1"]
-                 [io.nervous/kvlt                "0.1.1"]]
-  :plugins [[lein-cljsbuild "1.1.3"]
-            [lein-npm       "0.6.0"]
-            [lein-doo       "0.1.7-SNAPSHOT"]
-            [io.nervous/lein-cljs-lambda "0.5.1"]]
+                 [io.nervous/kvlt                "0.1.4"]]
+  :plugins [[lein-cljsbuild              "1.1.3"]
+            [lein-npm                    "0.6.2"]
+            [lein-doo                    "0.1.7"]
+            [io.nervous/lein-cljs-lambda "0.6.3"]]
   :npm {:dependencies [[source-map-support "0.4.0"]
-                       [aws-sdk "2.2.39"]]}
+                       [aws-sdk "2.3.15"]]}
   :source-paths ["src"]
   :cljs-lambda
   {:defaults {:role "arn:aws:iam::999371153281:role/cljs-lambda-default"
